20be7a5acef5f73571baeb8fe46be5d60a42e2e1
guoze123 <1030124836@qq.com>
2021-01-29 17:13:20 +0800

    13

--- book-manager/src/router/router.js
+++ book-manager/src/router/router.js
@@ -3,10 +3,10 @@ import Router from "vue-router";
 import Home from "../views/Home.vue";
 import reception from "../views/reception/Index.vue";
 import manager from "../views/admin/Index.vue";
-import login from '../views/Login.vue'
-import NProgress from "nprogress"
-import register from '../views/Register.vue'
-import notFound from '../views/404.vue';
+import login from "../views/Login.vue";
+import NProgress from "nprogress";
+import register from "../views/Register.vue";
+import notFound from "../views/404.vue";
 Vue.use(Router);
 
 let router = new Router({
@@ -15,62 +15,63 @@ let router = new Router({
       path: "/manager",
       name: "manager",
       component: manager,
+      redirect: "/manager/home",
       children: [
         {
           path: "/manager/home",
           name: "首页",
-          component: reception,
+          component: () =>
+            import(/* webpackChunkName: "about" */ "../views/admin/Home.vue")
         },
         {
           path: "/manager/bookManager",
           name: "图书管理",
-          component: reception,
+          component: () =>  import(/* webpackChunkName: "about" */ "../views/admin/BookManager.vue")
         },
         {
           path: "/manager/userManager",
           name: "人员管理",
-          component: reception,
+          component: () =>  import(/* webpackChunkName: "about" */ "../views/admin/UserManager.vue")
         },
         {
           path: "/manager/setTing",
           name: "参数设置",
-          component: reception,
           children: [
             {
               path: "/manager/setTing/pressManager",
               name: "出版社管理",
-              component: reception,
+              component: () =>  import(/* webpackChunkName: "about" */ "../views/admin/PressManager.vue")
             },
             {
               path: "/manager/setTing/bookType",
               name: "图书类型管理",
-              component: reception,
-            },
+              component: () =>  import(/* webpackChunkName: "about" */ "../views/admin/BookType.vue")
+            }
           ]
-        },
-      ],
+        }
+      ]
     },
     {
       path: "/reception",
       name: "reception",
-      component: reception,
+      component: reception
     },
     {
       path: "/login",
       name: "login",
-      component: login,
+      component: login
     },
     {
       path: "/register",
       name: "register",
-      component: register,
+      component: register
     },
     {
       path: "*",
       name: "notFound",
-      component: notFound,
-    },
-  ],
+      component: notFound
+    }
+  ]
 });
 // // 全局路由守卫
 router.beforeEach((to, form, next) => {
@@ -91,5 +92,4 @@ router.afterEach(() => {
   NProgress.done();
 });
 
-
-export default router
+export default router;
--- book-manager/src/views/admin/Home.vue
+++ book-manager/src/views/admin/Home.vue
@@ -20,4 +20,4 @@ export default {
   watch: {}
 };
 </script>
-<style lang="" scoped></style>
+<style lang="scss" scoped></style>
