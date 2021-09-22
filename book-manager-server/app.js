596eeef9f9e78003f370c091b368d846070618a4
guoze123 <1030124836@qq.com>
2021-09-22 22:11:14 +0800

    fix

--- book-manager-server/app.js
+++ book-manager-server/app.js
@@ -15,8 +15,8 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //设置跨域访问
